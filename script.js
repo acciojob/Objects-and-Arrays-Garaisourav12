fe1afa5655677327e7333d209d0a38a58bf1b857
SOURAV GARAI <101336956+Garaisourav12@users.noreply.github.com>
2023-09-20 01:55:39 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,10 +1,3 @@
-let players = ["John", "Bob", "Alice", "Poppy"];
-
-let person = {
-  name: "John Doe",
-  age: 80,
-};
-
 // Write your code here, make sure the name of variables is exactly same as mentioned in the problem statement
 const players = ["John", "Bob", "Alice", "Poppy"];
 const person = {
@@ -20,17 +13,6 @@ let team1 = players.slice();
 
 // Create a shallow copy of the object person
 let cap1 = Object.assign({}, person);
-
-// Test if the properties of person or the elements of players are modified
-players.push("New Player");
-person.name = "Updated Name";
-
-// Print the results to verify
-console.log("Original Players Array:", players);
-console.log("Reference (team) to Players Array:", team);
-console.log("Shallow Copy (team1) of Players Array:", team1);
-console.log("Shallow Copy (cap1) of Person Object:", cap1);
-
 
 window.players = players;
 window.person = person;
